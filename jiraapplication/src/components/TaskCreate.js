5a4b662d86797fd07ddf5da0de66cad745b1b91b
Yunuse <yunus.kupucu@gmaile.com>
2024-06-23 03:03:48 +0300

    task area

--- jiraapplication/src/components/TaskCreate.js
+++ jiraapplication/src/components/TaskCreate.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
+
 function TaskCreate() {
+  const [title, setTitle] = useState("");
+  const [taskDesc, setTaskDesc] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleTaskChange = (event) => {
+    setTaskDesc(event.target.value);
+  };
+
   return (
     <div className="task-create">
       <h3>Task Ekleyiniz ! </h3>
       <form className="task-form">
-        <label>Başlık:</label>
-        <input />
-        <label>Açıklama:</label>
-        <textarea />
-        <button>Oluştur</button>
+        <label className="task-label">Başlık:</label>
+        <input value={title} onChange={handleChange} className="task-text" />
+        <label className="task-label">Açıklama:</label>
+        <textarea
+          value={taskDesc}
+          onChange={handleTaskChange}
+          className="task-text"
+          rows={5}
+        />
+        <button className="task-button">Oluştur</button>
       </form>
     </div>
   );
