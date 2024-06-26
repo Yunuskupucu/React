fee04433df6bc117e9cbfab897bde591b17a3e79
Yunus Emre Küpücü <yunus.kupucu@gmail.com>
2024-06-26 15:07:54 +0300

    context api entegrasyonu yapıldı

--- jiraapplication/src/components/TaskShow.js
+++ jiraapplication/src/components/TaskShow.js
@@ -1,21 +1,28 @@
 import { useState } from "react";
 import TaskCreate from "./TaskCreate";
+import { useContext } from "react";
+import TasksContext from "../context/task";
 
-function TaskShow({ task, onDelete, onUpdate }) {
+function TaskShow({ task }) {
+  const { deleteTaskById, editTaskById } = useContext(TasksContext);
+
   const [showEdit, setShowEdit] = useState(false);
+
+  const handleDeleteClick = () => {
+    // onDelete(task.id);
+    deleteTaskById(task.id);
+  };
+
   const handleEditClick = () => {
     setShowEdit(true);
   };
 
-  const handleDeleteClick = () => {
-    onDelete(task.id);
-  };
   const handleSubmit = (id, updatedTitle, updatedTaskDesc) => {
     setShowEdit(false);
-    onUpdate(id, updatedTitle, updatedTaskDesc);
+    // onUpdate(id, updatedTitle, updatedTaskDesc);
+    editTaskById(id, updatedTitle, updatedTaskDesc);
   };
 
-  console.log(task);
   return (
     <div className="task-show">
       {showEdit ? (
